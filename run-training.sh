4c3cbcbcb464e386be623a47fde38da6bde5dd1c
Kiran Gangadharan <me@kirang.in>
2025-12-10 00:14:49 +0530

    Modify wrapper script to run training in daemon mode

--- run-training.sh
+++ run-training.sh
@@ -1,24 +1,4 @@
 #!/usr/bin/env bash
-# Wrapper script to run training with CUDA support
-# This adds the system NVIDIA driver path only for the training process
+# Wrapper script to run training in daemon mode
 
-set -e
-
-# Find system libcuda.so.1
-CUDA_DRIVER_PATH=""
-if [ -f /usr/lib/x86_64-linux-gnu/libcuda.so.1 ]; then
-    CUDA_DRIVER_PATH="/usr/lib/x86_64-linux-gnu"
-elif [ -f /usr/lib64/libcuda.so.1 ]; then
-    CUDA_DRIVER_PATH="/usr/lib64"
-else
-    echo "Warning: Could not find libcuda.so.1 in standard locations"
-    echo "Training may run on CPU instead of GPU"
-fi
-
-# Add CUDA driver path if found
-if [ -n "$CUDA_DRIVER_PATH" ]; then
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_DRIVER_PATH"
-fi
-
-# Run training with uv
-exec uv run train.py "$@"
+exec nohup uv run train.py >training.log 2>&1 &
