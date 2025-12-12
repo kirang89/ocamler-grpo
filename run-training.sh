5960c92944e07638b40bc1db1868a32e264bd524
Kiran Gangadharan <me@kirang.in>
2025-12-12 13:45:59 +0530

    Run dashboard server as part of the training flow

--- run-training.sh
+++ run-training.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# Wrapper script to run training in daemon mode
+# Wrapper script to run training and dashboard in daemon mode
 
-exec nohup uv run train.py >training.log 2>&1 &
+nohup uv run train.py >training.log 2>&1 &
+nohup uv run python dashboard/server.py >dashboard.log 2>&1 &
